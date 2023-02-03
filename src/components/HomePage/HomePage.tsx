9c0fe6eb5b66d3b1dddd48525f8c20669e85f305
= <=>
2023-02-03 13:33:22 -0600

    TO DO: Home Page component

---
+++ src/components/HomePage/HomePage.tsx
@@ -0,0 +1,21 @@
+import escudo from '../../assets/images/escudo.png'
+import { Box, Grid} from "@mui/material"
+import { LateralMenu} from "../LateralMenu/LateralMenu"
+
+export function HomePage(){
+    return (
+      <>
+        <Grid container>
+          <Grid item xs={3.5} lg={2}>
+            <LateralMenu />
+          </Grid>
+          <Grid item xs={8.5} sx={{justifyContent: "center"}}>
+              <img
+                src={escudo}
+                style={{ height: "50vh", backgroundColor: "#ff0"}}
+              ></img>
+          </Grid>
+        </Grid>
+      </>
+    );
+};
