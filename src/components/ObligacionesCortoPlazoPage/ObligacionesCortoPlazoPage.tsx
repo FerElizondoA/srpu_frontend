3ce683cf5873bc66cbeb7422181de362c5b8f94a
= <=>
2023-02-07 16:06:07 -0600

    Refactored tabs function

--- src/components/ObligacionesCortoPlazoPage/ObligacionesCortoPlazoPage.tsx
+++ src/components/ObligacionesCortoPlazoPage/ObligacionesCortoPlazoPage.tsx
@@ -6,50 +6,10 @@ import {
     Box
  } from "@mui/material"
 
-import { createTheme } from "@mui/material/styles";
-
+import useMediaQuery from "@mui/material/useMediaQuery";
 import { SyntheticEvent, useState } from "react";
 
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
-}
-
-function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-const theme = createTheme({
-  palette: {
-    secondary: {
-      main: "#AF8C55"
-    }
-  }
-});
-
-function a11yProps(index: number) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
-}
+import { SolicitudInscripcion } from "./Panels/SolicitudInscripcion";
 
 export const text = {
   regular: {
@@ -59,34 +19,56 @@ export const text = {
   },
 };
 
-export function ObligacionesCortoPlazoPage(){
+export function ObligacionesCortoPlazoPage() {
 
-    const [value, setValue] = useState(0);
+  const [tabIndex, setTabIndex] = useState(0);
+  
+  const handleChange = (event: SyntheticEvent, newTabIndex: number) => {
+    setTabIndex(newTabIndex);
+  }
 
-    const handleChange = (event: SyntheticEvent, newValue: number) => {
-      setValue(newValue);
-    }
+  const query ={
+    isXLg: useMediaQuery("(min-width: 1535px)")
+  }
 
-    return (
-      <Box>
-        <Tabs value={value} onChange={handleChange} centered sx={{ backgroundColor: "#F2F2F2" }}>
-          <Tab label="Información General" sx={text.regular} {...a11yProps(0)}></Tab>
-          <Tab label="Condiciones Financieras" sx={text.regular} {...a11yProps(1)}></Tab>
-          <Tab label="Documentación" sx={text.regular} {...a11yProps(2)}></Tab>
-          <Tab label="Solicitud de Inscripción" sx={text.regular} {...a11yProps(3)}></Tab>
-        </Tabs>
-        <TabPanel value={value} index={0}>
-          Panel 1
-        </TabPanel>
-        <TabPanel value={value} index={1}>
-          Panel 2
-        </TabPanel>
-        <TabPanel value={value} index={2}>
-          Panel 3
-        </TabPanel>
-        <TabPanel value={value} index={3}>
-          Panel 4
-        </TabPanel>
-      </Box>
-    );
-};
+  return (
+    <Box>
+      <Tabs
+        value={tabIndex}
+        onChange={handleChange}
+        centered
+        variant={query.isXLg ?  "standard" : "scrollable"}
+        scrollButtons="auto"
+      >
+        <Tab label="Encabezado" sx={text.regular}></Tab>
+        <Tab label="Información General" sx={text.regular}></Tab>
+        <Tab label="Condiciones Financieras" sx={text.regular}></Tab>
+        <Tab label="Documentación" sx={text.regular}></Tab>
+        <Tab label="Solicitud de Inscripción" sx={text.regular}></Tab>
+      </Tabs>
+      {tabIndex === 0 && (
+        <Grid container>
+          <Typography>Panel 1</Typography>
+        </Grid>
+      )}
+      {tabIndex === 1 && (
+          <Typography>Panel 2</Typography>
+      )}
+      {tabIndex === 2 && (
+        <Grid container>
+          <Typography>Panel 2</Typography>
+        </Grid>
+      )}
+      {tabIndex === 3 && (
+        <Grid container>
+          <Typography>Panel 3</Typography>
+        </Grid>
+      )}
+      {tabIndex === 4 && (
+        <Grid container>
+          <Typography>Panel 4</Typography>
+        </Grid>
+      )}
+    </Box>
+  );
+}
