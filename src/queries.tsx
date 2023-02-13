6f7a5a82a11a87c095b2b2b7336f8a17272ec650
= <=>
2023-02-13 16:05:47 -0600

    Added medium text query

--- src/queries.tsx
+++ src/queries.tsx
@@ -9,7 +9,20 @@ export const queries = {
     },
     "@media (min-width: 601px) and (max-width: 900px)": {
       // SM (small) screen
-      fontSize: "2ch",
+      fontSize: "1.5ch",
+    },
+  },
+  medium_text: {
+    fontSize: "2ch",
+    fontFamily: "MontserratMedium",
+    alignSelf: "center",
+    "@media (max-width: 600px)": {
+      // XS (extra small) screen
+      fontSize: "1rem",
+    },
+    "@media (min-width: 601px) and (max-width: 900px)": {
+      // SM (small) screen
+      fontSize: "1.5ch",
     },
   },
   bold_text: {
@@ -21,7 +34,7 @@ export const queries = {
     },
     "@media (min-width: 601px) and (max-width: 900px)": {
       // SM (small) screen
-      fontSize: "2ch",
+      fontSize: "1.5ch",
     },
   },
   italic_text: {
@@ -34,7 +47,7 @@ export const queries = {
     },
     "@media (min-width: 601px) and (max-width: 900px)": {
       // SM (small) screen
-      fontSize: "2ch",
+      fontSize: "1.5ch",
     },
   },
   icon: {
