377585e7f947051ce7ac517d1740b7a0c01e98df
MTYAlonso <japerez@cecapmex.com>
2023-02-15 09:58:05 -0600

    Added Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,72 @@
-import './App.css';
-import './Fonts.css'
+import React, { useLayoutEffect } from "react";
 
-import { 
-  BrowserRouter as Router, 
+import "./App.css";
+import "./Fonts.css";
+
+import {
+  // BrowserRouter as Router,
   Route,
-  createBrowserRouter, 
-  createRoutesFromElements,
-  RouterProvider
-} from 'react-router-dom';
+  // Link,
+  // createBrowserRouter,
+  // createRoutesFromElements,
+  // RouterProvider,
+  useNavigate,
+  Routes,
+} from "react-router-dom";
 
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { createTheme } from "@mui/material/styles";
+// import { CssBaseline } from "@mui/material";
+import { createTheme } from "@mui/material/styles"; //, ThemeProvider
 
-import { HomePage } from './components/HomePage/HomePage';
+import { HomePage } from "./components/HomePage/HomePage";
+import { continueSession, sessionValid } from "./validation";
 
 export const appTheme = createTheme({
   palette: {
-    primary:{
-      main: "#AF8C55"
-    }
+    primary: {
+      main: "#AF8C55",
+    },
   },
 });
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<HomePage />}>
-    </Route>
-  )
-);
+// const router = createBrowserRouter(
+//   createRoutesFromElements(<Route path="/" element={<HomePage />}></Route>)
+// );
 
 function App() {
+  const navigate = useNavigate();
+  const params = new URLSearchParams(window.location.search);
+  const jt = params.get("jwt") || null;
+  const IdApp = params.get("IdApp");
+
+  useLayoutEffect(() => {
+    if (jt !== null) {
+      sessionValid().then((r) => {
+        if ((r as boolean) === false) {
+          window.location.assign("http://10.200.4.106/");
+        } else if ((r as boolean) === true) {
+          setTimeout(() => {
+            localStorage.setItem("IdApp", IdApp as string);
+            navigate("../");
+          }, 2000);
+        }
+      });
+    } else {
+      continueSession().then((r) => {
+        if ((r as boolean) === false) {
+          window.location.assign("http://10.200.4.106/");
+        } else {
+          navigate("../");
+        }
+      });
+    }
+  }, [IdApp, jt, navigate]);
+
   return (
-    <ThemeProvider theme={appTheme}>
-      <CssBaseline enableColorScheme>
-        <RouterProvider router={router} />
-      </CssBaseline>
-    </ThemeProvider>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+      </Routes>
+    </>
   );
 }
 
