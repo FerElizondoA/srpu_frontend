c191c6461c551b4f99fc8533cb0dfab1eca2ae1d
MTYAlonso <japerez@cecapmex.com>
2023-02-20 17:23:33 -0600

    usuario srpu

--- routes/router.js
+++ routes/router.js
@@ -7,6 +7,7 @@ const { createFuenteDePago, getFuenteDePago, getDetailFuenteDePago, modifyFuente
 const router = express.Router();
 const { createInstitucionFinanciera, modifyInstitucionFinanciera, deleteInstitucionFinanciera, getInstitucionesFinancieras, getDetailInstitucionFinanciera } = require("../controllers/InstitucionesFinancieras.js");
 const { createObligadoSolidarioAval, getObligadoSolidarioAval, getDetailObligadoSolidarioAval, modifyObligadoSolidarioAval, deleteObligadoSolidarioAval } = require("../controllers/ObligadoSolidarioAval.js");
+const { getDetailUsuario } = require("../controllers/usuarios.js");
 
 
 
@@ -140,9 +141,13 @@ router.put("/modify-fuenteAlternaDePago", verifyToken.verifyJWT, (req, res) => {
   modifyFuenteAlternaDePago(req, res);
 });
 
-
 router.delete("/delete-fuenteAlternaDePago", verifyToken.verifyJWT, (req, res) => {
   deleteFuenteAlternaDePago(req, res);
+});
+
+//Usuario
+router.get("/detail-usuario", verifyToken.verifyJWT, (req, res) => {
+  getDetailUsuario(req, res);
 });
 
 
