27979defdcc2eb0c26e41c50f953061ca11451f4
= <=>
2023-02-28 16:07:20 -0600

    Added SolicitudInscripcion

--- src/components/ObligacionesCortoPlazoPage/Panels/SolicitudInscripcion.tsx
+++ src/components/ObligacionesCortoPlazoPage/Panels/SolicitudInscripcion.tsx
@@ -1,7 +1,142 @@
-
+import { useEffect } from "react";
+import {
+  Grid,
+  InputLabel,
+  TextField,
+  Divider,
+  Typography,
+  Checkbox,
+} from "@mui/material";
+import { queries } from "../../../queries";
 export function SolicitudInscripcion() {
-    return (
-        <>
-        </>
-    )
+  return (
+    <Grid item container>
+      <Grid
+        item
+        container
+        flexDirection="row"
+        mt={{ sm: 0, md: 0, lg: 0 }}
+        ml={{ sm: 10, md: 7, lg: window.innerWidth / 50 }}
+        spacing={{ xs: 2, md: 10, lg: 10 }}
+      >
+        <Grid item md={4.5} lg={4.5}>
+          <InputLabel sx={queries.medium_text}>
+            Nombre del servidor publico a quien va dirigido
+          </InputLabel>
+          <TextField
+            fullWidth
+            variant="standard"
+            sx={queries.medium_text}
+            InputLabelProps={{
+              style: {
+                fontFamily: "MontserratMedium",
+              },
+            }}
+            InputProps={{
+              style: {
+                fontFamily: "MontserratMedium",
+              },
+            }}
+          />
+        </Grid>
+        <Grid item md={4.5} lg={4.5}>
+          <InputLabel sx={queries.medium_text}>Cargo</InputLabel>
+          <TextField
+            fullWidth
+            variant="standard"
+            sx={queries.medium_text}
+            InputLabelProps={{
+              style: {
+                fontFamily: "MontserratMedium",
+              },
+            }}
+            InputProps={{
+              style: {
+                fontFamily: "MontserratMedium",
+              },
+            }}
+          />
+        </Grid>
+      </Grid>
+
+      <Grid
+        item
+        container
+        flexDirection="row"
+        mt={{ sm: 2, md: 0, lg: 0 }}
+        ml={{ sm: 10, md: 7, lg: window.innerWidth / 50 }}
+        spacing={{ md: 10, lg: 10 }}
+      >
+        <Grid item md={3} lg={3} xl={3}>
+          <InputLabel sx={queries.medium_text}>
+            Solicitante autorizado
+          </InputLabel>
+          <TextField
+            fullWidth
+            variant="standard"
+            sx={queries.medium_text}
+            InputLabelProps={{
+              style: {
+                fontFamily: "MontserratMedium",
+              },
+            }}
+            InputProps={{
+              style: {
+                fontFamily: "MontserratMedium",
+              },
+            }}
+          />
+        </Grid>
+
+        <Grid item md={3} lg={3} xl={3}>
+          <InputLabel sx={queries.medium_text}>
+            Documento de autorización
+          </InputLabel>
+          <TextField
+            fullWidth
+            variant="standard"
+            sx={queries.medium_text}
+            InputLabelProps={{
+              style: {
+                fontFamily: "MontserratMedium",
+              },
+            }}
+            InputProps={{
+              style: {
+                fontFamily: "MontserratMedium",
+              },
+            }}
+          />
+        </Grid>
+
+        <Grid item md={3} lg={3} xl={3}>
+          <InputLabel sx={queries.medium_text}>Identificación</InputLabel>
+          <TextField
+            fullWidth
+            variant="standard"
+            sx={queries.medium_text}
+            InputLabelProps={{
+              style: {
+                fontFamily: "MontserratMedium",
+              },
+            }}
+            InputProps={{
+              style: {
+                fontFamily: "MontserratMedium",
+              },
+            }}
+          />
+        </Grid>
+
+        <Grid item md={9} lg={9} xl={9}>
+          <Divider sx={queries.medium_text}>
+            Declaratorias aplicables
+            al financiamiento u obligación.
+          </Divider>
+        </Grid>
+      </Grid>
+
+     
+    </Grid>
+  );
 }
