85bfb138eee064838cb301dc310984f53860c7b8
= <=>
2023-03-08 16:27:29 -0600

    Added conditions for fetching

--- src/store/informacion_general.tsx
+++ src/store/informacion_general.tsx
@@ -1,27 +1,32 @@
 import { StateCreator } from "zustand";
-import dayjs, { Dayjs} from "dayjs";
 import axios from "axios";
 
-export interface InformacionGeneralSlice{
-    institucionCatalog: string[],
-    destinoCatalog: string[],
-    fechaContratacion: string,
-    plazoDias: number,
-    montoOriginal: number,
-    fechaVencimiento: string,
-    destino: string,
-    denominacion: string,
-    changeFechaContratacion: (newFechaContratacion: string) => void,
-    changePlazoDias: (newPlazoDias: number) => void,
-    changeMontoOriginal: (newMontoOriginal: number) => void,
-    changeFechaVencimiento: (newFechaVencimiento: string) => void,
-    changeDestino: (newDestino: string) => void,
-    changeDenominacion: (newDenominacion: string) => void,
-    fetchDestinos: () => void,
-    fetchInstituciones: () => void
+export interface InformacionGeneralSlice {
+  fetchedInstitucion: boolean;
+  fetchedDestino: boolean;
+  institucionCatalog: string[];
+  destinoCatalog: string[];
+  fechaContratacion: string;
+  plazoDias: number;
+  montoOriginal: number;
+  fechaVencimiento: string;
+  destino: string;
+  institucion: string;
+  denominacion: string;
+  changeFechaContratacion: (newFechaContratacion: string) => void;
+  changePlazoDias: (newPlazoDias: number) => void;
+  changeMontoOriginal: (newMontoOriginal: number) => void;
+  changeFechaVencimiento: (newFechaVencimiento: string) => void;
+  changeDestino: (newDestino: string) => void;
+  changeInstitucion: (newInstitucion: string) => void;
+  changeDenominacion: (newDenominacion: string) => void;
+  fetchDestinos: (fetch: boolean) => void;
+  fetchInstituciones: (fetch: boolean) => void;
 }
 
-export const createInformacionGeneralSlice: StateCreator<InformacionGeneralSlice> = (set) => ({
+export const createInformacionGeneralSlice: StateCreator<InformacionGeneralSlice> = (set,get) => ({
+    fetchedInstitucion: false,
+    fetchedDestino: false,
     institucionCatalog: [],
     destinoCatalog: [],
     fechaContratacion: "DD-MM-YYYY",
@@ -29,31 +34,51 @@ export const createInformacionGeneralSlice: StateCreator<InformacionGeneralSlice
     montoOriginal: 0,
     fechaVencimiento: "DD-MM-YYYY",
     destino: "",
-    denominacion: "",
+    institucion: "",
+    denominacion: "UDIS",
     changeFechaContratacion: (newFechaContratacion: string) => set(() =>  ({fechaContratacion: newFechaContratacion})),
     changePlazoDias: (newPlazoDias: number) => set(() => ({ plazoDias: newPlazoDias })),
     changeMontoOriginal: (newMontoOriginal: number) => set(() => ({ montoOriginal: newMontoOriginal })),
     changeFechaVencimiento: (newFechaVencimiento: string) => set(() => ({ fechaVencimiento: newFechaVencimiento})),
     changeDestino: (newDestino: string) => set(() => ({ destino: newDestino })),
+    changeInstitucion: (newInstitucion: string) => set(() => ({ institucion: newInstitucion })),
     changeDenominacion: (newDenominacion: string) => set(() => ({ denominacion: newDenominacion })),
     fetchDestinos: async () => {
-        const response = await axios.get("http://10.200.4.199:8000/api/get-destinos", {
-            headers: {
+        if (!get().fetchedDestino) {
+          console.log("fetchDestinos executed!");
+          const response = await axios.get(
+            "http://10.200.4.199:8000/api/get-destinos",
+            {
+              headers: {
                 Authorization: localStorage.getItem("jwtToken"),
-            },
-        })
-        response.data.data.forEach((e: any) => {
-            set((state) => ({ destinoCatalog: [...state.destinoCatalog, e]}))
-        })
+              },
+            }
+          );
+          response.data.data.forEach((e: any) => {
+            set((state) => ({
+              destinoCatalog: [...state.destinoCatalog, e.Destino],
+            }));
+          });
+          set(() => ({fetchedDestino: true}))
+        }
     },
     fetchInstituciones: async () => {
-        const response = await axios.get("http://10.200.4.199:8000/api/get-institucionesFinancieras", {
+      if (!get().fetchedInstitucion) {
+        console.log("fetchInstituciones executed!");
+        const response = await axios.get(
+          "http://10.200.4.199:8000/api/get-institucionesFinancieras",
+          {
             headers: {
-                Authorization: localStorage.getItem("jwtToken"),
+              Authorization: localStorage.getItem("jwtToken"),
             },
-        })
+          }
+        );
         response.data.data.forEach((e: any) => {
-            set((state) => ({ institucionCatalog: [...state.institucionCatalog, e]}))
-        })
+          set((state) => ({
+            institucionCatalog: [...state.institucionCatalog, e.Institucion],
+          }));
+        });
+      }
+      set(() => ({fetchedInstitucion: true}))
     }
 })
