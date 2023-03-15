96a4ecc61a7c21ed959b0c4fdf83185aece1aee0
= <=>
2023-03-15 13:32:22 -0600

    Moved the Dialog content to InformacionGeneral

--- src/components/ObligacionesCortoPlazoPage/Panels/InformacionGeneral.tsx
+++ src/components/ObligacionesCortoPlazoPage/Panels/InformacionGeneral.tsx
@@ -2,12 +2,23 @@ import * as React from "react";
 import {
   Grid,
   TextField,
-  Typography,
-  Button,
   InputLabel,
   InputAdornment,
-  Autocomplete
+  Autocomplete,
+  TableContainer,
+  Table,
+  TableHead,
+  TableBody,
+  TableSortLabel,
+  Checkbox
 } from "@mui/material";
+
+import {
+  StyledTableCell,
+  StyledTableRow,
+  ConfirmButton,
+  DeleteButton,
+} from "../../CustomComponents";
 
 import enGB from "date-fns/locale/en-GB";
 import { DatePicker } from "@mui/x-date-pickers";
@@ -15,22 +26,67 @@ import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { DateInput } from "../../CustomComponents";
 import { subDays, addDays } from "date-fns/esm";
-import { ObligadoSolidarioAval } from "../Dialogs/Dialog-IG-ObligadoSolidiario";
 import { queries } from "../../../queries";
 
 import { useCortoPlazoStore } from "../../../store/main";
 import { differenceInDays } from "date-fns";
 
+interface DataObligadoSalarial {
+  isSelected: boolean;
+  obligadosolidario: string;
+  tipoentepunlicoobligado: string;
+  entepublicoobligado: string;
+}
+
+interface Head {
+  id: keyof DataObligadoSalarial;
+  isNumeric: boolean;
+  label: string;
+}
+
+const heads: readonly Head[] = [
+  {
+    id: "isSelected",
+    isNumeric: false,
+    label: "Seleccion",
+  },
+  {
+    id: "isSelected",
+    isNumeric: false,
+    label: "Obligado solidario / aval",
+  },
+  {
+    id: "isSelected",
+    isNumeric: false,
+    label: "Tipo de ente público obligado",
+  },
+  {
+    id: "isSelected",
+    isNumeric: false,
+    label: "Ente público obligado",
+  },
+];
+
+function createDummyData(
+  obligadosolidario: string,
+  tipoentepunlicoobligado: string,
+  entepublicoobligado: string
+) {
+  return {
+    obligadosolidario,
+    tipoentepunlicoobligado,
+    entepublicoobligado,
+  };
+}
+
+const rows = [
+  createDummyData("ley federal", "Municipio", "Monterrey"),
+  createDummyData("opc1", "opc2", "opc3"),
+  createDummyData("ley federal", "Municipio", "Monterrey"),
+  createDummyData("opc1", "opc2", "opc3"),
+];
+
 export function InformacionGeneral() {
-
-  const [openObligado, changeOpenObligadoState] = React.useState(false)
-
-  const handleOpenObligadoState = React.useCallback(
-    (open:boolean) => {
-      changeOpenObligadoState(open);
-    },
-    []
-  );
 
   const institucion: string = useCortoPlazoStore(state => state.institucion);
   const changeInstitucion: Function = useCortoPlazoStore(state => state.changeInstitucion);
@@ -50,10 +106,20 @@ export function InformacionGeneral() {
   const changeFechaVencimiento: Function = useCortoPlazoStore(state => state.changeFechaVencimiento);
   const denominacion: string = useCortoPlazoStore(state => state.denominacion);
   const changeDenominacion: Function = useCortoPlazoStore(state => state.changeDenominacion);
+  const obligadoSolidarioAval: string = useCortoPlazoStore(state => state.obligadoSolidarioAval);
+  const changeObligadoSolidarioAval: Function = useCortoPlazoStore(state => state.changeObligadoSolidarioAval);
+  const obligadoSolidarioAvalCatalog: string[] = useCortoPlazoStore(state => state.obligadoSolidarioAvalCatalog);
+  const fetchObligadoSolidarioAval: Function = useCortoPlazoStore(state => state.fetchObligadoSolidarioAval);
+  const tipoEntePublicoObligado: string = useCortoPlazoStore(state => state.tipoEntePublicoObligado);
+  const changeTipoEntePublicoObligado: Function = useCortoPlazoStore(state => state.changeTipoEntePublicoObligado);
+  const tipoEntePublicoObligadoCatalog: string[] = useCortoPlazoStore(state => state.tipoEntePublicoObligadoCatalog);
+  const fetchTipoEntePublicoObligado: Function = useCortoPlazoStore(state => state.fetchTipoEntePublicoObligado);
 
   React.useEffect(() => {
     fetchDestinos();
     fetchInstituciones();
+    fetchObligadoSolidarioAval();
+    fetchTipoEntePublicoObligado();
     changePlazoDias(differenceInDays(new Date(fechaVencimiento), new Date(fechaContratacion)) + 1)
   }, [fechaContratacion, fechaVencimiento])
 
@@ -62,7 +128,7 @@ export function InformacionGeneral() {
       <Grid
         item
         container
-        mt={10}
+        mt={5}
         ml={{ xs: 5, sm: 10, md: 7, lg: window.innerWidth / 50 }}
         spacing={5}
       >
@@ -70,14 +136,17 @@ export function InformacionGeneral() {
           <InputLabel sx={queries.medium_text}>
             Fecha de Contratación
           </InputLabel>
-          <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={enGB}>
+          <LocalizationProvider
+            dateAdapter={AdapterDateFns}
+            adapterLocale={enGB}
+          >
             <DatePicker
               value={new Date(fechaContratacion)}
               onChange={(date) => changeFechaContratacion(date?.toString())}
               minDate={new Date(subDays(new Date(), 365))}
               maxDate={new Date()}
               slots={{
-                textField: DateInput
+                textField: DateInput,
               }}
             />
           </LocalizationProvider>
@@ -139,13 +208,16 @@ export function InformacionGeneral() {
       >
         <Grid item xs={3.5} md={3.5} lg={3}>
           <InputLabel sx={queries.medium_text}>Fecha de Vencimiento</InputLabel>
-          <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={enGB}>
+          <LocalizationProvider
+            dateAdapter={AdapterDateFns}
+            adapterLocale={enGB}
+          >
             <DatePicker
               value={new Date(fechaVencimiento)}
               onChange={(date) => changeFechaVencimiento(date?.toString())}
               minDate={new Date(addDays(new Date(fechaContratacion), 1))}
               slots={{
-                textField: DateInput
+                textField: DateInput,
               }}
             />
           </LocalizationProvider>
@@ -156,7 +228,7 @@ export function InformacionGeneral() {
           <Autocomplete
             fullWidth
             value={destino}
-            onChange={(event:any, text: string | null) => changeDestino(text)}
+            onChange={(event: any, text: string | null) => changeDestino(text)}
             options={destinoCatalog}
             renderInput={(params) => (
               <TextField
@@ -189,7 +261,7 @@ export function InformacionGeneral() {
         </Grid>
       </Grid>
 
-      <Grid item container mt={2} ml={window.innerWidth / 50 - 17} spacing={5}>
+      <Grid item container mt={2} ml={window.innerWidth / 50 - 13} spacing={5}>
         <Grid item lg={8.5} ml={window.outerWidth / 150}>
           <InputLabel sx={queries.medium_text}>
             Institución Financiera
@@ -197,7 +269,9 @@ export function InformacionGeneral() {
           <Autocomplete
             fullWidth
             value={institucion}
-            onChange={(event:any, text: string | null) => changeInstitucion(text)}
+            onChange={(event: any, text: string | null) =>
+              changeInstitucion(text)
+            }
             options={institucionCatalog}
             renderInput={(params) => (
               <TextField
@@ -214,27 +288,110 @@ export function InformacionGeneral() {
         item
         container
         mt={{ xs: 10, sm: 2, md: 5, lg: 2 }}
-        ml={{ xs: 5, sm: 10, md: 2, lg: 15 }}
+        ml={38}
         spacing={5}
       >
-        <Grid item lg={10}>
-          <Button
-            variant="outlined"
-            style={{
-              width: "100%",
-            }}
-            onClick={() => handleOpenObligadoState(!openObligado)}
-          >
-            <Grid item>
-              <Typography sx={queries.bold_text}>
-                Obligado Solidario / Aval
-              </Typography>
-            </Grid>
-          </Button>
-          <ObligadoSolidarioAval
-            handler={handleOpenObligadoState}
-            openState={openObligado}
+        <Grid item xs={3} md={3} lg={3}>
+          <InputLabel sx={queries.medium_text}>
+            Obligado Solidario / Aval
+          </InputLabel>
+          <Autocomplete
+            fullWidth
+            value={obligadoSolidarioAval}
+            options={obligadoSolidarioAvalCatalog}
+            onChange={(event: any, text: string | null) =>
+              changeObligadoSolidarioAval(text)
+            }
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                variant="standard"
+                sx={queries.medium_text}
+              />
+            )}
           />
+        </Grid>
+
+        <Grid item xs={3} md={3} lg={3}>
+          <InputLabel sx={queries.medium_text}>
+            Tipo de ente público obligado
+          </InputLabel>
+          <Autocomplete
+            fullWidth
+            value={tipoEntePublicoObligado}
+            options={tipoEntePublicoObligadoCatalog}
+            onChange={(event: any, text: string | null) =>
+              changeTipoEntePublicoObligado(text)
+            }
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                variant="standard"
+                sx={queries.medium_text}
+              />
+            )}
+          />
+        </Grid>
+
+        <Grid item xs={3} md={3} lg={3}>
+          <InputLabel sx={queries.medium_text}>
+            Ente público obligado
+          </InputLabel>
+          <TextField
+            fullWidth
+            variant="standard"
+            sx={queries.medium_text}
+            InputLabelProps={{
+              style: {
+                fontFamily: "MontserratMedium",
+              },
+            }}
+            InputProps={{
+              style: {
+                fontFamily: "MontserratMedium",
+              },
+            }}
+          />
+        </Grid>
+
+        <Grid item container>
+          <Grid item lg={9}>
+            <TableContainer sx={{ maxHeight: "200px" }}>
+              <Table>
+                <TableHead>
+                  {heads.map((head) => (
+                    <StyledTableCell key={head.id}>
+                      <TableSortLabel>{head.label}</TableSortLabel>
+                    </StyledTableCell>
+                  ))}
+                </TableHead>
+                <TableBody>
+                  {rows.map((row) => (
+                    <StyledTableRow>
+                      <StyledTableCell padding="checkbox">
+                        <Checkbox />
+                      </StyledTableCell>
+                      <StyledTableCell component="th" scope="row">
+                        {row.entepublicoobligado.toString()}
+                      </StyledTableCell>
+                      <StyledTableCell component="th">
+                        {row.obligadosolidario.toString()}
+                      </StyledTableCell>
+                      <StyledTableCell component="th">
+                        {row.tipoentepunlicoobligado.toString()}
+                      </StyledTableCell>
+                    </StyledTableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Grid>
+          <Grid item md={6} lg={4.5} mt={1}>
+            <ConfirmButton variant="outlined">AGREGAR</ConfirmButton>
+          </Grid>
+          <Grid item md={6} lg={4.5} mt={1}>
+            <DeleteButton variant="outlined">ELIMINAR</DeleteButton>
+          </Grid>
         </Grid>
       </Grid>
     </Grid>
