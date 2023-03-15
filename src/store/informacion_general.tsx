31bd428f1ca5167641b0c0d3692fc347f04a0f70
= <=>
2023-03-15 13:31:58 -0600

    Added obligado solidario aval & tipo ente publico obligado

--- src/store/informacion_general.tsx
+++ src/store/informacion_general.tsx
@@ -1,18 +1,21 @@
 import { StateCreator } from "zustand";
-import { differenceInDays } from "date-fns";
-import { useCortoPlazoStore } from "./main";
 import axios from "axios";
-import { addDays } from "date-fns/esm";
 
 export interface InformacionGeneralSlice {
   fetchedInstitucion: boolean;
   fetchedDestino: boolean;
+  fetchedObligadoSolidarioAval: boolean;
+  fetchedTipoEntePublicoObligado: boolean;
   institucionCatalog: string[];
   destinoCatalog: string[];
+  obligadoSolidarioAvalCatalog: string[];
+  tipoEntePublicoObligadoCatalog: string[];
   plazoDias: number;
   montoOriginal: number;
   fechaVencimiento: string;
   destino: string;
+  tipoEntePublicoObligado: string;
+  obligadoSolidarioAval: string;
   institucion: string;
   denominacion: string;
   changePlazoDias: (newPlazoDias: number) => void;
@@ -21,20 +24,30 @@ export interface InformacionGeneralSlice {
   changeDestino: (newDestino: string) => void;
   changeInstitucion: (newInstitucion: string) => void;
   changeDenominacion: (newDenominacion: string) => void;
-  fetchDestinos: (fetch: boolean) => void;
-  fetchInstituciones: (fetch: boolean) => void;
+  changeObligadoSolidarioAval: (newObligadoSolidarioAval: string) => void;
+  changeTipoEntePublicoObligado: (newTipoEntePublicoObligado: string) => void;
+  fetchDestinos: () => void;
+  fetchInstituciones: () => void;
+  fetchTipoEntePublicoObligado: () => void;
+  fetchObligadoSolidarioAval: () => void;
 }
 
 export const createInformacionGeneralSlice: StateCreator<InformacionGeneralSlice> = (set,get) => ({
     fetchedInstitucion: false,
     fetchedDestino: false,
+    fetchedObligadoSolidarioAval: false,
+    fetchedTipoEntePublicoObligado: false,
     institucionCatalog: [],
     destinoCatalog: [],
+    obligadoSolidarioAvalCatalog: [],
+    tipoEntePublicoObligadoCatalog: [],
     plazoDias: 0,
     montoOriginal: 0,
     fechaVencimiento: "",
     destino: "",
     institucion: "",
+    tipoEntePublicoObligado: "",
+    obligadoSolidarioAval: "",
     denominacion: "Pesos",
     changePlazoDias: (newPlazoDias: number) => set(() => ({ plazoDias: newPlazoDias })),
     changeMontoOriginal: (newMontoOriginal: number) => set(() => ({ montoOriginal: newMontoOriginal })),
@@ -42,6 +55,8 @@ export const createInformacionGeneralSlice: StateCreator<InformacionGeneralSlice
     changeDestino: (newDestino: string) => set(() => ({ destino: newDestino })),
     changeInstitucion: (newInstitucion: string) => set(() => ({ institucion: newInstitucion })),
     changeDenominacion: (newDenominacion: string) => set(() => ({ denominacion: newDenominacion })),
+    changeObligadoSolidarioAval: (newObligadoSolidarioAval: string) => set(() => ({ obligadoSolidarioAval: newObligadoSolidarioAval})),
+    changeTipoEntePublicoObligado: (newTipoEntePublicoObligado: string) => set(() => ({ tipoEntePublicoObligado: newTipoEntePublicoObligado})),
     fetchDestinos: async () => {
         if (!get().fetchedDestino) {
           console.log("fetchDestinos executed!");
@@ -81,5 +96,43 @@ export const createInformacionGeneralSlice: StateCreator<InformacionGeneralSlice
         });
       }
       set(() => ({fetchedInstitucion: true}))
+    },
+    fetchTipoEntePublicoObligado: async () => {
+      if (!get().fetchedTipoEntePublicoObligado) {
+        console.log("fetchTipoEntePublicoObligado executed!");
+        const response = await axios.get(
+          "http://10.200.4.199:8000/api/get-tiposEntePublico",
+          {
+            headers: {
+              Authorization: localStorage.getItem("jwtToken"),
+            },
+          }
+        );
+        response.data.data.forEach((e: any) => {
+          set((state) => ({
+            tipoEntePublicoObligadoCatalog: [...state.tipoEntePublicoObligadoCatalog, e.Descripcion],
+          }));
+        });
+      }
+      set(() => ({fetchedTipoEntePublicoObligado: true}))
+    },
+    fetchObligadoSolidarioAval: async () => {
+      if (!get().fetchedObligadoSolidarioAval) {
+        console.log("fetchObligadoSolidarioAval executed!");
+        const response = await axios.get(
+          "http://10.200.4.199:8000/api/get-obligadoSolidarioAval",
+          {
+            headers: {
+              Authorization: localStorage.getItem("jwtToken"),
+            },
+          }
+        );
+        response.data.data.forEach((e: any) => {
+          set((state) => ({
+            obligadoSolidarioAvalCatalog: [...state.obligadoSolidarioAvalCatalog, e.Descripcion],
+          }));
+        });
+      }
+      set(() => ({fetchedObligadoSolidarioAval: true}))
     }
 })
