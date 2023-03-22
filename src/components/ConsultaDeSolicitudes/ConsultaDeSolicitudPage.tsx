7730e427b1e53962b0ffbd22cd522ef01e2aaf1a
Israel Mendez <mimensdez@cecapmex.com>
2023-03-22 12:08:00 -0600

    primera parte funcional consulta

--- src/components/ConsultaDeSolicitudes/ConsultaDeSolicitudPage.tsx
+++ src/components/ConsultaDeSolicitudes/ConsultaDeSolicitudPage.tsx
@@ -24,18 +24,17 @@ import Paper from "@mui/material/Paper";
 import InputBase from "@mui/material/InputBase";
 import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
+import { useEffect, useState } from "react";
+import { element } from "prop-types";
 
 interface Data {
-  Número_de_Solicitud: string;
-  Monto_original_Contratado: number;
-  Denominacíon: string;
-  Estatus_de_la_solicitud: string;
-  Tipo_de_solicitud: string;
-
-  Tipo_de_ente_público_obligado: string;
   Institucion_financiera: string;
-  Solicitante_autorizado: string;
-  Cargo_de_solicitante: string;
+  Tipo_de_ente_público_obligado: string;
+  Clave_de_inscripcion: string;
+  Estatus: string;
+  Fecha_de_contratacion_desde: Date;
+  Monto_original_contratado: number;
+  //search: string
 }
 
 interface Head {
@@ -45,196 +44,35 @@ interface Head {
 }
 const heads: readonly Head[] = [
   {
-    id: "Número_de_Solicitud",
-    isNumeric: false,
-    label: "Número De Solicitud",
-  },
-
-  {
-    id: "Monto_original_Contratado",
+    id: "Institucion_financiera",
     isNumeric: true,
-    label: "Monto original contratado",
+    label: "Institucion financiera",
   },
-
-  {
-    id: "Denominacíon",
-    isNumeric: true,
-    label: "Denominacíon",
-  },
-
-  {
-    id: "Estatus_de_la_solicitud",
-    isNumeric: true,
-    label: "Estatus de la solicitud",
-  },
-
-  {
-    id: "Tipo_de_solicitud",
-    isNumeric: true,
-    label: "Tipo de solicitud",
-  },
-
   {
     id: "Tipo_de_ente_público_obligado",
     isNumeric: true,
     label: "Tipo de ente público obligado",
   },
-
   {
-    id: "Institucion_financiera",
+    id: "Estatus",
     isNumeric: true,
-    label: "Institucion financiera",
+    label: "Estatus",
   },
-
   {
-    id: "Solicitante_autorizado",
+    id: "Clave_de_inscripcion",
     isNumeric: true,
-    label: "Solicitante autorizado",
+    label: "Clave de inscripcion",
   },
-
   {
-    id: "Cargo_de_solicitante",
+    id: "Monto_original_contratado",
     isNumeric: true,
-    label: "Cargo de solicitante",
+    label: "Monto original contratado",
   },
-];
-
-function createDummyData(
-  Número_de_Solicitud: string,
-  Monto_original_Contratado: number,
-  Denominacíon: string,
-  Estatus_de_la_solicitud: string,
-  Tipo_de_solicitud: string,
-  Tipo_de_ente_público_obligado: string,
-  Institucion_financiera: string,
-  Solicitante_autorizado: string,
-  Cargo_de_solicitante: string
-) {
-  return {
-    Número_de_Solicitud,
-    Monto_original_Contratado,
-    Denominacíon,
-    Estatus_de_la_solicitud,
-    Tipo_de_solicitud,
-    Tipo_de_ente_público_obligado,
-    Institucion_financiera,
-    Solicitante_autorizado,
-    Cargo_de_solicitante,
-  };
-}
-
-const rows = [
-  createDummyData(
-    "121-DC",
-    10000000,
-    "pesos",
-    "Aprobado",
-    "Corto Plazo",
-    "Municipio",
-    "Bvba Banorte empresarial",
-    "Jesus Echeverria Rodrigues",
-    "Tesorero del Estado de Nuevo Leon"
-  ),
-  createDummyData(
-    "122-AB",
-    35000000,
-    "udis",
-    "Aprobado",
-    "Corto Plazo",
-    "Municipio",
-    "Santander Empresarial",
-    "Marcelo Ebrad Martinez",
-    "Secretaria de Finanzas de Jaurez"
-  ),
-  createDummyData(
-    "121-DC",
-    10000000,
-    "pesos",
-    "Aprobado",
-    "Corto Plazo",
-    "Municipio",
-    "Bvba Banorte empresarial",
-    "Jesus Echeverria Rodrigues",
-    "Tesorero del Estado de Nuevo Leon"
-  ),
-  createDummyData(
-    "122-AB",
-    35000000,
-    "udis",
-    "Aprobado",
-    "Corto Plazo",
-    "Municipio",
-    "Santander Empresarial",
-    "Marcelo Ebrad Martinez",
-    "Secretaria de Finanzas de Jaurez"
-  ),
-  createDummyData(
-    "121-DC",
-    10000000,
-    "pesos",
-    "Aprobado",
-    "Corto Plazo",
-    "Municipio",
-    "Bvba Banorte empresarial",
-    "Jesus Echeverria Rodrigues",
-    "Tesorero del Estado de Nuevo Leon"
-  ),
-  createDummyData(
-    "122-AB",
-    35000000,
-    "udis",
-    "Aprobado",
-    "Corto Plazo",
-    "Municipio",
-    "Santander Empresarial",
-    "Marcelo Ebrad Martinez",
-    "Secretaria de Finanzas de Jaurez"
-  ),
-  createDummyData(
-    "121-DC",
-    10000000,
-    "pesos",
-    "Aprobado",
-    "Corto Plazo",
-    "Municipio",
-    "Bvba Banorte empresarial",
-    "Jesus Echeverria Rodrigues",
-    "Tesorero del Estado de Nuevo Leon"
-  ),
-  createDummyData(
-    "122-AB",
-    35000000,
-    "udis",
-    "Aprobado",
-    "Corto Plazo",
-    "Municipio",
-    "Santander Empresarial",
-    "Marcelo Ebrad Martinez",
-    "Secretaria de Finanzas de Jaurez"
-  ),
-  createDummyData(
-    "121-DC",
-    10000000,
-    "pesos",
-    "Aprobado",
-    "Corto Plazo",
-    "Municipio",
-    "Bvba Banorte empresarial",
-    "Jesus Echeverria Rodrigues",
-    "Tesorero del Estado de Nuevo Leon"
-  ),
-  createDummyData(
-    "122-AB",
-    35000000,
-    "udis",
-    "Aprobado",
-    "Corto Plazo",
-    "Municipio",
-    "Santander Empresarial",
-    "Marcelo Ebrad Martinez",
-    "Secretaria de Finanzas de Jaurez"
-  ),
-  //createDummyData("prueba1", 10000000, ),
+  {
+    id: "Fecha_de_contratacion_desde",
+    isNumeric: true,
+    label: "Fecha de contratacion desde",
+  },
 ];
 
 // const StyledTableCell = styled(TableCell)(({ theme }) => ({
@@ -260,8 +98,97 @@ const rows = [
 //     border: 0,
 //   },
 // }));
+const ejemplos = [
+  {
+    Institucion_financiera: "Banorte",
+    Monto_original_contratado: 2000000,
+    Tipo_de_ente_público_obligado: "Municipio",
+    Clave_de_inscripcion: "123-rg",
+    Estatus: "completa",
+    Fecha_de_contratacion_desde: "12/03/2023", 
+  },
+  {
+    Institucion_financiera: "Santandar",
+    Monto_original_contratado: 3000000,
+    Tipo_de_ente_público_obligado: "Municipio",
+    Clave_de_inscripcion: "434-ph",
+    Estatus: "por terminar",
+    Fecha_de_contratacion_desde: "06/01/2023",
+  },
+  {
+    Institucion_financiera: "Banco Azteca",
+    Monto_original_contratado: 4500000,
+    Tipo_de_ente_público_obligado: "Municipio",
+    Clave_de_inscripcion: "988-tf",
+    Estatus: "incompleta",
+    Fecha_de_contratacion_desde: "01/02/2023",
+  },
+  {
+    Institucion_financiera: "Citibanamex",
+    Monto_original_contratado: 8500000,
+    Tipo_de_ente_público_obligado: "Municipio",
+    Clave_de_inscripcion: "788-ap",
+    Estatus: "por terminar",
+    Fecha_de_contratacion_desde: "01/01/2023",
+  },
+];
+
+
 
 export function ConsultaDeSolicitudPage() {
+  //console.log("hola soy el objeto", ejemplos);
+
+  // const [word, setWord] = useState("");
+  // const [filterDisplay, setFilterDisplay] = useState([])
+  // const [busqueda] = useState([ejemplos])
+  // const handleChange = () =>{
+  //   //setWord();
+  //   let oldList = ejemplos.map(row => {
+  //     return {Instituto: row.Institucion_financiera, Monto: row.Monto_original_contratado, Ente_público: row.Tipo_de_ente_público_obligado,
+  //             Clave: row.Clave_de_inscripcion, Estatus: row.Estatus, Fecha: row.Fecha_de_contratacion_desde
+  //     };
+  //   });
+  //   if (word !== ""){
+  //     let newList = [];
+
+  //     newList = oldList.filter(row =>
+  //       row.Instituto.includes(word.toLowerCase())
+  //       );
+  //       setFilterDisplay(newList);
+  //   }else{
+  //     setFilterDisplay(ejemplos)
+  //   };
+
+  // }
+
+  const [datos, setDatos] = useState([]);
+  const [datostabla, setDatosTabla] =useState([]);
+  const [busqueda, setBusqueda] = useState("");
+
+  const handleChange =(e: React.ChangeEvent<HTMLInputElement>)=>{
+    setBusqueda(e.target.value)
+    filtrar(e.target.value);
+    
+  }
+
+  const filtrar = (terminoBusqueda: string) =>{
+    let ResultadoBusqueda = ejemplos.filter((elemento)=>{
+      if (elemento.Clave_de_inscripcion.toString().toLocaleLowerCase().includes(terminoBusqueda.toLocaleLowerCase())
+        || elemento.Estatus.toString().toLocaleLowerCase().includes(terminoBusqueda.toLocaleLowerCase())
+        || elemento.Fecha_de_contratacion_desde.toString().toLocaleLowerCase().includes(terminoBusqueda.toLocaleLowerCase())
+        || elemento.Institucion_financiera.toString().toLocaleLowerCase().includes(terminoBusqueda.toLocaleLowerCase())
+        || elemento.Monto_original_contratado.toString().toLocaleLowerCase().includes(terminoBusqueda.toLocaleLowerCase())
+        || elemento.Tipo_de_ente_público_obligado.toString().toLocaleLowerCase().includes(terminoBusqueda.toLocaleLowerCase())
+      )
+      {
+        console.log("elemento", elemento);
+        return elemento;
+        
+        
+      }
+    })
+  }
+
   return (
     <Grid container direction="column">
       <Grid item width={"100%"}>
@@ -288,10 +215,12 @@ export function ConsultaDeSolicitudPage() {
           <InputBase
             sx={{ ml: 1, flex: 1 }}
             placeholder="Buscar"
+            value={busqueda}
+            onChange={handleChange}
             //inputProps={{ "aria-label": "search google maps" }}
           />
           <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
-            <SearchIcon />
+            <SearchIcon  onClick = {() =>handleChange}/>
           </IconButton>
         </Paper>
       </Grid>
@@ -307,26 +236,10 @@ export function ConsultaDeSolicitudPage() {
               ))}
             </TableHead>
             <TableBody>
-              {rows.map((row) => (
+              {ejemplos.map((row) => (
                 <StyledTableRow>
-                  <StyledTableCell component="th" scope="row">
-                    {row.Número_de_Solicitud.toString()}
-                  </StyledTableCell>
-
-                  <StyledTableCell component="th" scope="row">
-                    {row.Monto_original_Contratado.toString()}
-                  </StyledTableCell>
-
-                  <StyledTableCell component="th" scope="row">
-                    {row.Denominacíon.toString()}
-                  </StyledTableCell>
-
-                  <StyledTableCell component="th" scope="row">
-                    {row.Estatus_de_la_solicitud.toString()}
-                  </StyledTableCell>
-
-                  <StyledTableCell component="th" scope="row">
-                    {row.Tipo_de_solicitud.toString()}
+                  <StyledTableCell component="th" scope="row" >
+                    {row.Institucion_financiera.toString()}
                   </StyledTableCell>
 
                   <StyledTableCell component="th" scope="row">
@@ -334,16 +247,21 @@ export function ConsultaDeSolicitudPage() {
                   </StyledTableCell>
 
                   <StyledTableCell component="th" scope="row">
-                    {row.Institucion_financiera.toString()}
+                    {row.Estatus.toString()}
                   </StyledTableCell>
 
                   <StyledTableCell component="th" scope="row">
-                    {row.Solicitante_autorizado.toString()}
+                    {row.Clave_de_inscripcion.toString()}
                   </StyledTableCell>
 
                   <StyledTableCell component="th" scope="row">
-                    {row.Cargo_de_solicitante.toString()}
+                    {row.Monto_original_contratado.toString()}
                   </StyledTableCell>
+
+                  <StyledTableCell component="th" scope="row">
+                    {row.Fecha_de_contratacion_desde}
+                  </StyledTableCell>
+                  
                 </StyledTableRow>
               ))}
             </TableBody>
