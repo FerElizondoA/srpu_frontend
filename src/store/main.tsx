a596f9c4db3c560b2e2a040c15d124a91133a36e
Josvan Gonzalez <=>
2023-03-22 18:19:43 -0600

    Added PagosCapitalSlice and converted all Slices into one

--- src/store/main.tsx
+++ src/store/main.tsx
@@ -2,10 +2,16 @@ import create from "zustand";
 
 import { createEncabezadoSlice, EncabezadoSlice } from "./encabezado";
 import { createInformacionGeneralSlice, InformacionGeneralSlice } from "./informacion_general";
-import {createSolicitudInscripcionSlice, SolicitudInscripcionSlice} from "./solicitud_inscripcion";
-export const useCortoPlazoStore = create<EncabezadoSlice & InformacionGeneralSlice & SolicitudInscripcionSlice>()((...x) => ({
+import { createSolicitudInscripcionSlice, SolicitudInscripcionSlice } from "./solicitud_inscripcion";
+import { createPagosCapitalSlice, PagosCapitalSlice } from "./pagos_capital";
+
+type AllSlice = EncabezadoSlice & InformacionGeneralSlice
+                & SolicitudInscripcionSlice & PagosCapitalSlice;
+
+export const useCortoPlazoStore = create<AllSlice>()((...x) => ({
     ...createEncabezadoSlice(...x),
     ...createInformacionGeneralSlice(...x),
-    ...createSolicitudInscripcionSlice(...x)
+    ...createSolicitudInscripcionSlice(...x),
+    ...createPagosCapitalSlice(...x)
 }))
 
