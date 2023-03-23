bf723bef12b69bab0fd56df6a43adc460686f700
Josvan Gonzalez <=>
2023-03-23 15:18:59 -0600

    Added pending slices

--- src/store/main.tsx
+++ src/store/main.tsx
@@ -4,14 +4,19 @@ import { createEncabezadoSlice, EncabezadoSlice } from "./encabezado";
 import { createInformacionGeneralSlice, InformacionGeneralSlice } from "./informacion_general";
 import { createSolicitudInscripcionSlice, SolicitudInscripcionSlice } from "./solicitud_inscripcion";
 import { createPagosCapitalSlice, PagosCapitalSlice } from "./pagos_capital";
+import { createTasaEfectivaSlice, TasaEfectivaSlice } from "./tasa_efectiva";
+import { createCondicionFinancieraSlice, CondicionFinancieraSlice } from "./condicion_financiera";
 
 type AllSlice = EncabezadoSlice & InformacionGeneralSlice
-                & SolicitudInscripcionSlice & PagosCapitalSlice;
+                & SolicitudInscripcionSlice & PagosCapitalSlice
+                & TasaEfectivaSlice & CondicionFinancieraSlice;
 
 export const useCortoPlazoStore = create<AllSlice>()((...x) => ({
     ...createEncabezadoSlice(...x),
     ...createInformacionGeneralSlice(...x),
     ...createSolicitudInscripcionSlice(...x),
-    ...createPagosCapitalSlice(...x)
+    ...createPagosCapitalSlice(...x),
+    ...createTasaEfectivaSlice(...x),
+    ...createCondicionFinancieraSlice(...x)
 }))
 
