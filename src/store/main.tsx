d43d0e0512bce1f53628e275226600853287a775
Israel Mendez <mimensdez@cecapmex.com>
2023-04-04 14:45:07 -0600

    documentos en zustans

--- src/store/main.tsx
+++ src/store/main.tsx
@@ -6,10 +6,11 @@ import { createSolicitudInscripcionSlice, SolicitudInscripcionSlice } from "./so
 import { createPagosCapitalSlice, PagosCapitalSlice } from "./pagos_capital";
 import { createTasaEfectivaSlice, TasaEfectivaSlice } from "./tasa_efectiva";
 import { createCondicionFinancieraSlice, CondicionFinancieraSlice } from "./condicion_financiera";
+import { createDocumentoSlice, DocumentosSlice } from "./documentacion";
 
 export type SolicitudSlice = EncabezadoSlice & InformacionGeneralSlice
                 & SolicitudInscripcionSlice & PagosCapitalSlice
-                & TasaEfectivaSlice & CondicionFinancieraSlice;
+                & TasaEfectivaSlice & CondicionFinancieraSlice & DocumentosSlice;
 
 export const useCortoPlazoStore = create<SolicitudSlice>()((...x) => ({
     ...createEncabezadoSlice(...x),
@@ -17,6 +18,7 @@ export const useCortoPlazoStore = create<SolicitudSlice>()((...x) => ({
     ...createSolicitudInscripcionSlice(...x),
     ...createPagosCapitalSlice(...x),
     ...createTasaEfectivaSlice(...x),
-    ...createCondicionFinancieraSlice(...x)
+    ...createCondicionFinancieraSlice(...x),
+    ...createDocumentoSlice(...x)
 }))
 
