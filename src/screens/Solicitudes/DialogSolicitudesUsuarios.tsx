ebdddad4adf0531cd37423013c03cbe510243d56
FerElizondoA <felizondo@cecapmex.com>
2023-04-18 16:18:20 -0600

    merge con marlon

--- src/screens/Solicitudes/DialogSolicitudesUsuarios.tsx
+++ src/screens/Solicitudes/DialogSolicitudesUsuarios.tsx
@@ -10,7 +10,14 @@ import {
   ListItem,
   Typography,
   DialogActions,
-  Button
+  Button,
+  TableContainer,
+  Paper,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody
 } from "@mui/material";
 import { IComentarios } from "./ISoliciudes";
 import { queriesSolicitud } from "./queriesSolicitudes";
@@ -28,9 +35,23 @@ export const DialogSolicitudesUsuarios = ({
 
   useEffect(() => {
     console.log('comentario', comentarios);
-    
- 
   }, [])
+
+  
+function createData(
+  name: string,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number,
+) {
+  return { name, calories, fat, carbs, protein };
+}
+
+  const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+
+  ];
   
 return (
  
@@ -42,36 +63,30 @@ return (
       <DialogContent>
         <Grid>
           {comentarios?.map((dato, index) => (
-            <List
-              sx={{ width: "100%", maxWidth: 350, bgcolor: "background.paper" }}
-            >
-              <ListItem alignItems="flex-start">
-                <Box>
-                  <Box
-                    sx={{
-                      display: "flex",
-                      justifyContent: "space-between",
-                    }}
+            <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 550 }} size="small" aria-label="a dense table">
+              <TableHead>
+                <TableRow>
+                  <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                  <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                  <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.map((row) => (
+                  <TableRow
+                    key={row.name}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                   >
-                    <Box sx={queriesSolicitud.boxContenidoBuscador}>
-                      <Typography
-                        sx={queriesSolicitud.typograhyCampoBuscador}
-                        // color={index === indexSelect ? "#af8c55 " : "black"}
-                      >
-                        USUARIO:{" "}
-                      </Typography>
-                      <Typography
-                        sx={queriesSolicitud.typograhyResultadoBuscador}
-                        // color={index === indexSelect ? "#af8c55 " : "black"}
-                      >
-                        {dato.Comentario.toUpperCase()}
-                      </Typography>
-                    </Box>
-                  </Box>
-                </Box>
-              </ListItem>
-              <Divider component="li" />
-            </List>
+                    <TableCell align="right">{row.fat}</TableCell>
+                    <TableCell align="right">{row.carbs}</TableCell>
+                    <TableCell align="right">{row.protein}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+
           ))}
         </Grid>
         <DialogContentText></DialogContentText>
