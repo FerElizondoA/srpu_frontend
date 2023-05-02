e708e83996c03feba79103c79c81a19e4c3711ba
Israel Mendez <mimensdez@cecapmex.com>
2023-05-02 17:16:34 -0600

    Full apis

---
+++ src/components/APIS/APIS Cortoplazo/APISDocumentacion.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import Swal from "sweetalert2";
+
+export function getTiposDocumentos(setState: Function) {
+  axios({
+    method: 'get',
+    url: process.env.REACT_APP_APPLICATION_BACK + '/api/get-tiposDocumentosCortoPlazo',
+    data: {},
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: localStorage.getItem("jwtToken") || "",
+    },
+  })
+    .then(({ data }) => {
+      setState(data.data);
+    })
+    .catch((error) => {
+      Swal.fire({
+        icon: "error",
+        title: "Mensaje",
+        text: "(" + error.response.status + ") " + error.response.data.msg,
+      })
+    });
+}
---
+++ src/components/APIS/APIS Cortoplazo/APISInformacionGeneral.ts
@@ -0,0 +1,71 @@
+import axios from "axios";
+import Swal from "sweetalert2";
+
+export async function getDestinos (){
+    await axios({
+        method: 'get',
+        url: 'http://10.200.4.200:8000' + '/api/get-destinos',
+        data: {},
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: localStorage.getItem("jwtToken") || "",
+          },
+      })
+      .then( ({data})=> {
+            return  data;
+      })
+      .catch( (error)=> {
+        Swal.fire({
+          icon: "error",
+          title: "Mensaje",
+          text: "(" + error.response.status + ") " + error.response.data.msg,
+        });
+      });
+}
+
+export  function getObligadoSolidarioAval (setState:Function){
+     axios({
+        method: 'get',
+        url: 'http://10.200.4.200:8000' + '/api/get-obligadoSolidarioAval',
+        data: {},
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: localStorage.getItem("jwtToken") || "",
+          },
+      })
+      .then( ({data})=> {
+      
+        
+           //setState(data)
+            return  data;
+      })
+      .catch( (error)=> {
+        Swal.fire({
+          icon: "error",
+          title: "Mensaje",
+          text: "(" + error.response.status + ") " + error.response.data.msg,
+        });
+      });
+}
+
+export  function getSolicitudes (setState:Function){
+  axios({
+     method: 'get',
+     url: 'http://10.200.4.200:8000' + '/api/get-solicitudes',
+     data: {},
+     headers: {
+         "Content-Type": "application/json",
+         Authorization: localStorage.getItem("jwtToken") || "",
+       },
+   })
+   .then( ({data})=> {
+         setState(data.data)
+   })
+   .catch( (error)=> {
+     Swal.fire({
+       icon: "error",
+       title: "Mensaje",
+       text: "(" + error.response.status + ") " + error.response.data.msg,
+     });
+   });
+}
