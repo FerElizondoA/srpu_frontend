e3c8e60cee1429704a760a3b0e20757827206cdb
MTYAlonso <japerez@cecapmex.com>
2023-05-15 16:32:16 -0600

    dialog

--- src/components/ObligacionesCortoPlazoPage/Panels/SolicitudInscripcion.tsx
+++ src/components/ObligacionesCortoPlazoPage/Panels/SolicitudInscripcion.tsx
@@ -23,6 +23,7 @@ import { ConfirmacionDescargaSolicitud } from "../Dialogs/DialogEnviarSolicitud"
 import { ConfirmacionBorradorSolicitud } from "../Dialogs/DialogGuardarBorrador";
 import { ConfirmacionCancelarSolicitud } from "../Dialogs/DialogCancelarSolicitud";
 import { DialogSolicitarModificacion } from "../Dialogs/DialogSolicitarModificacion";
+import Swal from "sweetalert2";
 
 interface Head {
   label: string;
@@ -67,16 +68,230 @@ export function SolicitudInscripcion() {
   const changeReglasAplicables: Function = useCortoPlazoStore(
     (state) => state.changeReglasAplicables
   );
-
-  const reglasAplicables: string[] = useCortoPlazoStore((state)=>state.reglasAplicables)
-
+  const reglasAplicables: string[] = useCortoPlazoStore(
+    (state) => state.reglasAplicables
+  );
   const getReglas: Function = useCortoPlazoStore((state) => state.getReglas);
 
   useEffect(() => {
     getReglas();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
   let err = 0;
+
+  const Toast = Swal.mixin({
+    toast: true,
+    showConfirmButton: true,
+    // confirmButtonColor: "red",
+    confirmButtonText: "De acuerdo",
+    // timer: 2000,
+    // timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener("mouseenter", Swal.stopTimer);
+      toast.addEventListener("mouseleave", Swal.resumeTimer);
+    },
+  });
+  const InfoFaltante = () => {
+    errores = [];
+
+    const state = useCortoPlazoStore.getState();
+    const solicitud: any = {
+      encabezado: state.encabezado,
+      MontoOriginalContratado: state.informacionGeneral.monto,
+      PlazoDias: state.informacionGeneral.plazo,
+      Destino: state.informacionGeneral.destino.Descripcion,
+      Denominacion: state.informacionGeneral.denominacion,
+      InstitucionFinanciera:
+        state.informacionGeneral.institucionFinanciera.Descripcion,
+    };
+    /////////////////// Por definir /////////////////////
+    let entePublicoObligado = "";
+    let obligadoSolidario = "";
+    let tipoEntePublicoObligado = "";
+
+    for (let i = 0; i < state.tablaObligadoSolidarioAval.length; i++) {
+      const item = state.tablaObligadoSolidarioAval[0];
+      entePublicoObligado = item.entePublicoObligado;
+      obligadoSolidario = item.obligadoSolidario;
+      tipoEntePublicoObligado = item.tipoEntePublicoObligado;
+    }
+    ///////////////////   Condiciones Financieras /////////////////////
+    let importe = 0;
+    let numeroDePago = 0;
+    let PeriocidadDePago = "";
+    let diasEjercicio = "";
+    let tasaEfectiva = "";
+    let comisiones: any = [];
+    let TasaDeInteres: any = [];
+
+    for (let i = 0; i < state.tablaCondicionesFinancieras.length; i++) {
+      const item = state.tablaCondicionesFinancieras[0];
+      importe = item.disposicion.importe;
+      numeroDePago = item.pagosDeCapital.numeroDePago;
+      PeriocidadDePago = item.pagosDeCapital.periodicidadDePago;
+      TasaDeInteres = item.tasaInteres;
+      diasEjercicio = item.diasEjercicio;
+      tasaEfectiva = item.tasaEfectiva;
+      comisiones = item.comisiones;
+    }
+    if (
+      solicitud.PlazoDias === undefined ||
+      solicitud.PlazoDias === 0 ||
+      /^[\s]*$/.test(solicitud.PlazoDias)
+    ) {
+      err = 1;
+      errores.push(
+        "Sección <strong>Información General</strong>:El Plazo a Días no puede ser  0."
+      );
+    }
+
+    if (
+      solicitud.MontoOriginalContratado === undefined ||
+      solicitud.MontoOriginalContratado === 0 ||
+      /^[\s]*$/.test(solicitud.MontoOriginalContratado)
+    ) {
+      err = 1;
+
+      errores.push(
+        "Sección <strong>Información General</strong>:Ingrese un Monto original contratado valido."
+      );
+    }
+    if (
+      solicitud.Destino === undefined ||
+      solicitud.Destino === "" ||
+      /^[\s]*$/.test(solicitud.Destino)
+    ) {
+      err = 1;
+
+      errores.push(
+        "Sección <strong>Información General</strong>:Seleccione  el Destino."
+      );
+    }
+    if (
+      solicitud.InstitucionFinanciera === undefined ||
+      solicitud.InstitucionFinanciera === "" ||
+      /^[\s]*$/.test(solicitud.InstitucionFinanciera)
+    ) {
+      err = 1;
+
+      errores.push(
+        "Sección <strong>Información General</strong>:Seleccione la Institución Financiera."
+      );
+    }
+
+    if (
+      state.tablaCondicionesFinancieras[0] === undefined ||
+      state.tablaCondicionesFinancieras[0] === null
+    ) {
+      err = 1;
+
+      errores.push(
+        "Sección <strong>Condiciones Financieras</strong>:Agruege al menos una Condicion Financiera."
+      );
+    }
+
+    if (TasaDeInteres[0] === undefined || TasaDeInteres[0].tasa === "") {
+      err = 1;
+
+      errores.push(
+        "Sección <strong>Condiciones Financieras</strong>:Agruege al menos una Tasa De Interés."
+      );
+    }
+    if (importe === undefined || importe === 0) {
+      err = 1;
+
+      errores.push(
+        "Sección <strong>Condiciones Financieras</strong>:Ingrese el Importe."
+      );
+    }
+    if (numeroDePago === undefined || numeroDePago === 0) {
+      err = 1;
+
+      errores.push(
+        "Sección <strong>Condiciones Financieras</strong>:Ingrese el Numero de pagos."
+      );
+    }
+    if (
+      PeriocidadDePago === undefined ||
+      PeriocidadDePago === "" ||
+      /^[\s]*$/.test(PeriocidadDePago)
+    ) {
+      err = 1;
+
+      errores.push(
+        "Sección <strong>Condiciones Financieras</strong>:Seleccione la periocidad de pago."
+      );
+    }
+    if (
+      diasEjercicio === undefined ||
+      diasEjercicio === "" ||
+      /^[\s]*$/.test(diasEjercicio)
+    ) {
+      err = 1;
+
+      errores.push(
+        "Sección <strong>Condiciones Financieras</strong>:Seleccione los Díaz del Ejercicio."
+      );
+    }
+    if (
+      tasaEfectiva === undefined ||
+      tasaEfectiva === "" ||
+      /^[\s]*$/.test(tasaEfectiva)
+    ) {
+      err = 1;
+      errores.push(
+        "Sección <strong>Condiciones Financieras</strong>:Ingrese la tasa Efectiva."
+      );
+    }
+
+    if (comisiones[0] === undefined || comisiones[0].tipoDeComision === "") {
+      errores.push(
+        "Sección <strong>Condiciones Financieras</strong>:Agregue al menos una comision."
+      );
+    }
+
+    if (
+      state.reglasAplicables[0] === undefined ||
+      state.reglasAplicables[0] === ""
+    ) {
+      errores.push(
+        "Sección <strong>Solicitud de Inscripción</strong>:Agregue al menos una regla."
+      );
+    }
+
+    if (err === 0) {
+      setOpenDialogEnviar(!openDialogEnviar);
+    } else {
+      Toast.fire({
+        icon: "error",
+        html: `
+      <div style="height:100%; width:100%">
+      <h3>Se han encontrado los siguientes errores:</h3>
+      <div style="text-align: left; margin-left: 10px; color: red; height: 300px; overflow: auto;">
+    <small>
+    <strong>
+    *</strong>${errores.join("<br><strong>*</strong>")}
+    </small>
+    </div>
+    </div>`,
+      });
+    }
+  };
+
+  let arrReglas: Array<string> = [];
+  arrReglas = reglasAplicables;
+
+  const removeRegla = (descripcion: string) => {
+    let aux: Array<string> = [];
+    arrReglas.map((regla, index) => {
+      if (regla !== descripcion) {
+        aux.push(regla);
+      }
+    });
+    arrReglas = aux;
+    changeReglasAplicables(arrReglas);
+  };
 
   return (
     <Grid item container>
@@ -216,8 +431,9 @@ export function SolicitudInscripcion() {
                         <StyledTableRow key={index}>
                           <StyledTableCell padding="checkbox">
                             <Checkbox
-                            checked ={reglasAplicables.includes(row.Descripcion)}
-                            
+                              checked={reglasAplicables.includes(
+                                row.Descripcion
+                              )}
                               disabled={
                                 (checkObj[1] === true && index === 2) ||
                                 (checkObj[2] === true && index === 1) ||
@@ -225,7 +441,6 @@ export function SolicitudInscripcion() {
                                 (checkObj[4] === true && index === 3)
                               }
                               onChange={(v) => {
-                                
                                 v.target.checked
                                   ? setCheckObj({ ...checkObj, [index]: true })
                                   : setCheckObj({
@@ -234,11 +449,9 @@ export function SolicitudInscripcion() {
                                     });
 
                                 v.target.checked
-                                  ? (reglasSeleccionadas[index] =
-                                      row.Descripcion)
-                                  : delete reglasSeleccionadas[index];
-
-                                changeReglasAplicables(reglasSeleccionadas);
+                                  ? arrReglas.push(row.Descripcion)
+                                  : removeRegla(row.Descripcion);
+                                changeReglasAplicables(arrReglas);
                               }}
                             />
                           </StyledTableCell>
@@ -277,10 +490,11 @@ export function SolicitudInscripcion() {
 
             {localStorage.getItem("Rol") === "Verificador" ? (
               <Button
-              sx={queries.buttonContinuar}
-              onClick={() => {
-                setOpenDialogModificacion(!openDialogModificacion);
-              }}>
+                sx={queries.buttonContinuar}
+                onClick={() => {
+                  setOpenDialogModificacion(!openDialogModificacion);
+                }}
+              >
                 Solicitar Modificacion
               </Button>
             ) : null}
@@ -297,7 +511,7 @@ export function SolicitudInscripcion() {
             <Button
               sx={queries.buttonContinuar}
               onClick={() => {
-                setOpenDialogEnviar(!openDialogEnviar);
+                InfoFaltante();
               }}
             >
               {localStorage.getItem("Rol") === "Verificador"
