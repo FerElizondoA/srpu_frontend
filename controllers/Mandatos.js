19f4bec5f6c6c4f6ce2470bd53d162c63adb3dd2
FerElizondoA <felizondo@cecapmex.com>
2023-09-18 16:08:09 -0600

    mandatos

--- controllers/Mandatos.js
+++ controllers/Mandatos.js
@@ -61,63 +61,63 @@ module.exports = {
   },
 
   // DETALLE POR ID
-  getDetailMandato: (req, res) => {
-    const IdDescripcion = req.body.IdDescripcion;
-    if (IdDescripcion == null || /^[\s]*$/.test(IdDescripcion)) {
-      return res.status(409).send({
-        error: "Ingrese IdDescripcion.",
-      });
-    }
+  // getDetailMandato: (req, res) => {
+  //   const IdDescripcion = req.body.IdDescripcion;
+  //   if (IdDescripcion == null || /^[\s]*$/.test(IdDescripcion)) {
+  //     return res.status(409).send({
+  //       error: "Ingrese IdDescripcion.",
+  //     });
+  //   }
 
-    db.query(`CALL sp_DetalleMandato('${IdDescripcion}')`, (err, result) => {
-      if (err) {
-        return res.status(500).send({
-          error: "Error",
-        });
-      }
-      if (result.length) {
-        const data = result[0][0];
-        if (data.error) {
-          return res.status(409).send({
-            result: data,
-          });
-        }
-        return res.status(200).send({
-          data,
-        });
-      } else {
-        return res.status(409).send({
-          error: "¡Sin Información!",
-        });
-      }
-    });
-  },
+  //   db.query(`CALL sp_DetalleMandato('${IdDescripcion}')`, (err, result) => {
+  //     if (err) {
+  //       return res.status(500).send({
+  //         error: "Error",
+  //       });
+  //     }
+  //     if (result.length) {
+  //       const data = result[0][0];
+  //       if (data.error) {
+  //         return res.status(409).send({
+  //           result: data,
+  //         });
+  //       }
+  //       return res.status(200).send({
+  //         data,
+  //       });
+  //     } else {
+  //       return res.status(409).send({
+  //         error: "¡Sin Información!",
+  //       });
+  //     }
+  //   });
+  // },
 
   //MODIFICA POR ID
+  
   modifyMandato: (req, res) => {
-    const IdDescripcion = req.body.IdDescripcion;
-    const Descripcion = req.body.Descripcion;
-    const IdUsuarioModificador = req.body.IdUsuario;
+    const IdMandato = req.body.IdMandato;
+    const IdUsuario = req.body.IdUsuario;
+    const FechaMandato = req.body.FechaMandato;
+    const Mandatario = req.body.Mandatario;
+    const MunicipioMandante = req.body.MunicipioMandante;
+    const OrganismoMandante = req.body.OrganismoMandante;
+    const TipoMovimiento = req.body.TipoMovimiento;
+    const SoporteDocumental = req.body.SoporteDocumental;
 
-    if (IdDescripcion == null || /^[\s]*$/.test(IdDescripcion)) {
+    if (IdMandato == null || /^[\s]*$/.test(IdMandato)) {
       return res.status(409).send({
         error: "Ingrese Id",
       });
     }
 
-    if (Descripcion == null || /^[\s]*$/.test(Descripcion)) {
-      return res.status(409).send({
-        error: "Ingrese Nuevo Mandato",
-      });
-    }
-
-    if (IdUsuarioModificador == null || /^[\s]*$/.test(IdUsuarioModificador)) {
+    if (IdUsuario == null || /^[\s]*$/.test(IdUsuarioModificador)) {
       return res.status(409).send({
         error: "Ingrese Id usuario modificador",
       });
     } else {
       db.query(
-        `CALL sp_ModificaMandato('${IdDescripcion}','${Descripcion}','${IdUsuarioModificador}')`,
+        `CALL sp_ModificaMandato('${IdMandato}','${IdUsuario}','${FechaMandato}','${Mandatario}','${MunicipioMandante}','${OrganismoMandante}','${TipoMovimiento}','${SoporteDocumental}')`,
         (err, result) => {
           if (err) {
             return res.status(500).send({
