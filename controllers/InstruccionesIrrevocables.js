b401b3d2fda830431aa09a020643d1585c58f277
FerElizondoA <felizondo@cecapmex.com>
2023-10-20 17:58:02 -0600

    Clasificacion, instrucciones y respecto 20/10/2023

---
+++ controllers/InstruccionesIrrevocables.js
@@ -0,0 +1,163 @@
+const db = require("../config/db.js");
+
+module.exports = {
+  //CAMBIA VARIABLES
+  createInstruccion: (req, res) => {
+    const IdUsuario = req.body.IdUsuario;
+    const NumeroCuenta = req.body.NumeroCuenta;
+    const CLABE = req.body.CLABE;
+    const Banco = req.body.Banco;
+    const MecanismoPago = req.body.MecanismoPago;
+    const TipoMovimiento = req.body.TipoMovimiento;
+    const EntePublico = req.body.EntePublico;
+
+    if (
+      (NumeroCuenta == null || /^[\s]*$/.test(NumeroCuenta)) &&
+      NumeroCuenta.length() <= 255
+    ) {
+      return res.status(409).send({
+        error: "Ingrese Descripcion válida.",
+      });
+    }
+    if (
+      (IdUsuario == null || /^[\s]*$/.test(IdUsuario)) &&
+      IdUsuario.length() <= 36
+    ) {
+      return res.status(409).send({
+        error: "Ingrese Id usuario válido.",
+      });
+    } else {
+      db.query(
+        `CALL sp_AgregarInstruccionIrrevocable('${IdUsuario}', '${NumeroCuenta}' , '${CLABE}', '${Banco}', '${MecanismoPago}', '${TipoMovimiento}', '${EntePublico}')`,
+        (err, result) => {
+          console.log(err)
+          if (err) {
+            return res.status(500).send({
+              error: "Error",
+            });
+          }
+          if (result.length) {
+            const data = result[0][0];
+            if (data.error) {
+              return res.status(409).send({
+                result: data,
+              });
+            }
+            return res.status(200).send({
+              data,
+            });
+          } else {
+            return res.status(409).send({
+              error: "¡Sin Información!",
+            });
+          }
+        }
+      );
+    }
+  },
+
+  modifyInstruccion: (req, res) => {
+    const IdUsuario = req.body.IdUsuario;
+    const IdInstruccion = req.body.IdInstruccion;
+    const NumeroCuenta = req.body.NumeroCuenta;
+    const CLABE = req.body.CLABE;
+    const Banco = req.body.Banco;
+    const MecanismoPago = req.body.MecanismoPago;
+    const TipoMovimiento = req.body.TipoMovimiento;
+    const EntePublico = req.body.EntePublico;
+
+    if (IdIntruccion == null || /^[\s]*$/.test(IdIntruccion)) {
+      return res.status(409).send({
+        error: "Ingrese Id",
+      });
+    }
+
+    if (IdUsuario == null || /^[\s]*$/.test(IdUsuario)) {
+      return res.status(409).send({
+        error: "Ingrese Nuevo Clave de inscripcion",
+      });
+    } else {
+      db.query(
+        `CALL sp_ModificaInstruccionIrrevocable('${IdUsuario}', '${IdInstruccion}', '${NumeroCuenta}' , '${CLABE}', '${Banco}', '${MecanismoPago}', '${TipoMovimiento}', '${EntePublico}')`,
+        (err, result) => {
+          console.log(err)
+          if (err) {
+            return res.status(500).send({
+              error: "Error",
+            });
+          }
+          if (result.length) {
+            const data = result[0][0];
+            if (data.error) {
+              return res.status(409).send({
+                result: data,
+              });
+            }
+            return res.status(200).send({
+              result: data,
+            });
+          } else {
+            return res.status(409).send({
+              error: "¡Sin Información!",
+            });
+          }
+        }
+      );
+    }
+  },
+
+  deleteInstruccion: (req, res) => {
+    const IdInstruccion = req.body.IdInstruccion;
+    const IdUsuarioModificador = req.body.IdUsuario;
+    db.query(
+      `CALL sp_BajaLogicaInstruccionesIrrevocables('${IdInstruccion}', '${IdUsuarioModificador}')`,
+      (err, result) => {
+        //console.log(req.body)
+        if (err) {
+          //console.log(err)
+          return res.status(500).send({
+            error: "Error",
+          });
+        }
+        if (result.length) {
+          const data = result[0][0];
+          if (data.error) {
+            return res.status(409).send({
+              result: data,
+            });
+          }
+          return res.status(200).send({
+            result: data,
+          });
+        } else {
+          return res.status(409).send({
+            error: "¡Sin Información!",
+          });
+        }
+      }
+    );
+  },
+
+  getInstrucciones: (req, res) => {
+    db.query(`CALL sp_ListadoInstruccionesIrrevocables()`,
+     (err, result) => {
+      if (err) {
+        return res.status(500).send({
+          error: "Error",
+        });
+      }
+
+      if (result.length) {
+        const data = result[0];
+        return res.status(200).send({
+          data,
+        });
+      } else {
+        return res.status(409).send({
+          error: "¡Sin Información!",
+        });
+      }
+    });
+  },
+
+};
---
+++ controllers/Respecto.js
@@ -0,0 +1,155 @@
+const db = require("../config/db.js");
+
+module.exports = {
+    createRespecto: (req, res) => {
+        const IdUsuario = req.body.IdUsuario;
+        const Descripcion = req.body.Descripcion;
+    
+        if (
+          (Descripcion == null || /^[\s]*$/.test(Descripcion)) &&
+          Descripcion.length() <= 255
+        ) {
+          return res.status(409).send({
+            error: "Ingrese Descripcion válido.",
+          });
+        }
+        if (
+          (IdUsuario == null || /^[\s]*$/.test(IdUsuario)) &&
+          IdUsuario.length() <= 36
+        ) {
+          return res.status(409).send({
+            error: "Ingrese Id usuario válido.",
+          });
+        } else {
+          db.query(
+            `CALL sp_AgregarRespecto('${IdUsuario}', '${Descripcion}' )`,
+            (err, result) => {
+              if (err) {
+                return res.status(500).send({
+                  error: "Error",
+                });
+              }
+              if (result.length) {
+                const data = result[0][0];
+                if (data.error) {
+                  return res.status(409).send({
+                    result: data,
+                  });
+                }
+                return res.status(200).send({
+                  data,
+                });
+              } else {
+                return res.status(409).send({
+                  error: "¡Sin Información!",
+                });
+              }
+            }
+          );
+        }
+      },
+    
+      //LISTADO COMPLETO
+      getRespecto: (req, res) => {
+        db.query(`CALL sp_ListadoRespecto()`, (err, result) => {
+          if (err) {
+            return res.status(500).send({
+              error: "Error",
+            });
+          }
+    
+          if (result.length) {
+            const data = result[0];
+            return res.status(200).send({
+              data,
+            });
+          } else {
+            return res.status(409).send({
+              error: "¡Sin Información!",
+            });
+          }
+        });
+      },
+    
+      //MODIFICA POR ID
+      modifyRespecto: (req, res) => {
+        const IdDescripcion = req.body.IdDescripcion;
+        const Descripcion = req.body.Descripcion;
+        const IdUsuarioModificador = req.body.IdUsuario;
+    
+        if (IdDescripcion == null || /^[\s]*$/.test(IdDescripcion)) {
+          return res.status(409).send({
+            error: "Ingrese Id",
+          });
+        }
+    
+        if (Descripcion == null || /^[\s]*$/.test(Descripcion)) {
+          return res.status(409).send({
+            error: "Ingrese Nuevo Clave de inscripcion",
+          });
+        }
+    
+        if (IdUsuarioModificador == null || /^[\s]*$/.test(IdUsuarioModificador)) {
+          return res.status(409).send({
+            error: "Ingrese Id usuario modificador",
+          });
+        } else {
+          db.query(
+            `CALL sp_ModificaRespecto('${IdDescripcion}','${Descripcion}','${IdUsuarioModificador}')`,
+            (err, result) => {
+              if (err) {
+                return res.status(500).send({
+                  error: "Error",
+                });
+              }
+              if (result.length) {
+                const data = result[0][0];
+                if (data.error) {
+                  return res.status(409).send({
+                    result: data,
+                  });
+                }
+                return res.status(200).send({
+                  result: data,
+                });
+              } else {
+                return res.status(409).send({
+                  error: "¡Sin Información!",
+                });
+              }
+            }
+          );
+        }
+      },
+    
+      //BORRADO LOGICO
+      deleteRespecto: (req, res) => {
+        const IdDescripcion = req.query.IdDescripcion;
+        const IdUsuarioModificador = req.query.IdUsuario;
+        db.query(
+          `CALL sp_BajaLogicaRespecto('${IdDescripcion}', '${IdUsuarioModificador}')`,
+          (err, result) => {
+            if (err) {
+              return res.status(500).send({
+                error: "Error",
+              });
+            }
+            if (result.length) {
+              const data = result[0][0];
+              if (data.error) {
+                return res.status(409).send({
+                  result: data,
+                });
+              }
+              return res.status(200).send({
+                result: data,
+              });
+            } else {
+              return res.status(409).send({
+                error: "¡Sin Información!",
+              });
+            }
+          }
+        );
+      },
+};
