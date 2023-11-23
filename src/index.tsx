4fa837164305cf43a52438ebd85f5e991e85c8a9
MTYAlonso <japerez@cecapmex.com>
2023-11-23 09:56:10 -0600

    a

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
+import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
-import { HashRouter } from "react-router-dom";
 
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
