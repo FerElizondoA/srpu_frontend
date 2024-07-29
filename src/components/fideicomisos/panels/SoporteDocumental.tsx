abbd30b2ec96325fecb822b1bd2d9403b811acd3
PedroPardoG <prpardo@cecapmex.com>
2024-07-29 11:38:07 -0600

    aplicaciond e env

--- src/components/fideicomisos/panels/SoporteDocumental.tsx
+++ src/components/fideicomisos/panels/SoporteDocumental.tsx
@@ -141,7 +141,7 @@ export function SoporteDocumentalFideicomiso() {
 
   useEffect(() => {
     if (idFideicomiso !== "") {
-      listFile(`/SRPU/FIDEICOMISOS/${idFideicomiso}/`, setArr).then(() => {
+      listFile(process.env.REACT_APP_APPLICATION_RUTA_ARCHIVOS+`/FIDEICOMISOS/${idFideicomiso}/`, setArr).then(() => {
         setLoading(false);
       });
     }
